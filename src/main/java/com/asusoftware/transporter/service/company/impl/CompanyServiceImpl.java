b187c6c09cbce75ecfa015da5ede00e2f006259f
Antonio <web.antonio.popa@gmail.com>
2020-12-25 17:06:02 +0100

    Initial commit

--- src/main/java/com/asusoftware/transporter/service/company/impl/CompanyServiceImpl.java
+++ src/main/java/com/asusoftware/transporter/service/company/impl/CompanyServiceImpl.java
@@ -1,11 +1,15 @@
 package com.asusoftware.transporter.service.company.impl;
 
+import com.asusoftware.transporter.model.Address;
+import com.asusoftware.transporter.model.Company;
+import com.asusoftware.transporter.model.dto.address.AddressDto;
 import com.asusoftware.transporter.model.dto.company.CreateCompanyDto;
-import com.asusoftware.transporter.service.company.CompanyCreator;
+import com.asusoftware.transporter.repository.CompanyRepository;
 import com.asusoftware.transporter.service.company.CompanyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -15,10 +19,31 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class CompanyServiceImpl implements CompanyService {
 
-    private final CompanyCreator companyCreator;
+    private final CompanyRepository companyRepository;
 
     @Override
     public UUID create(CreateCompanyDto createCompanyDto) {
-        return companyCreator.create(createCompanyDto);
+        Company company = companyRepository.save(mapCompanyDtoToEntity(createCompanyDto));
+        return company.getId();
+    }
+
+    private Company mapCompanyDtoToEntity(CreateCompanyDto createCompanyDto) {
+        Company company = new Company();
+        company.setName(createCompanyDto.getName());
+        company.setAddress(Optional.ofNullable(createCompanyDto.getAddress()).map(this::mapAddressDtoToEntity).orElse(null));
+        company.setDescription(createCompanyDto.getDescription());
+        company.setImage(createCompanyDto.getImage());
+        return company;
+    }
+
+    private Address mapAddressDtoToEntity(AddressDto addressDto) {
+        Address address = new Address();
+        address.setCity(addressDto.getCity());
+        address.setCountry(addressDto.getCountry());
+        address.setStreet(addressDto.getStreet());
+        address.setStreetNumber(addressDto.getStreetNumber());
+        address.setDetails(addressDto.getDetails());
+        address.setZip(addressDto.getZip());
+        return address;
     }
 }
